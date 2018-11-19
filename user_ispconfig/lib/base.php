2761414c867089563d226d256be1b425b50acb79
Michael Fürmann <michael@spicyweb.de>
2018-11-19 11:31:55 +0100

    formats code

--- user_ispconfig/lib/base.php
+++ user_ispconfig/lib/base.php
@@ -4,7 +4,9 @@
  * This file is licensed under the Affero General Public License version 3 or
  * later.
  */
+
 namespace OCA\user_ispconfig;
+
 use \OC_DB;
 
 /**
@@ -20,197 +22,206 @@ use \OC_DB;
  * @license  http://www.gnu.org/licenses/agpl AGPL
  * @link     https://spicyhub.de/spicy-web/nextcloud-user-ispconfig
  */
-abstract class Base extends \OC\User\Backend{
+abstract class Base extends \OC\User\Backend
+{
 
-    /**
-     * Delete a user
-     *
-     * @param string $uid The username of the user to delete
-     * @return bool
-     * @throws \OC\DatabaseException
-     */
-	public function deleteUser($uid) {
-        OC_DB::executeAudited(
-            'DELETE FROM `*PREFIX*accounts` WHERE `uid` = ?',
-            array($uid)
-        );
-        OC_DB::executeAudited(
-            'DELETE FROM `*PREFIX*preferences` WHERE `userid` = ?',
-            array($uid)
-        );
-        OC_DB::executeAudited(
-            'DELETE FROM `*PREFIX*group_user` WHERE `uid` = ?',
-            array($uid)
-        );
-        OC_DB::executeAudited(
-            'DELETE FROM `*PREFIX*users_ispconfig` WHERE `uid` = ?',
-            array($uid)
-        );
-		return true;
-	}
+  /**
+   * Delete a user
+   *
+   * @param string $uid The username of the user to delete
+   * @return bool
+   * @throws \OC\DatabaseException
+   */
+  public function deleteUser($uid)
+  {
+    OC_DB::executeAudited(
+        'DELETE FROM `*PREFIX*accounts` WHERE `uid` = ?',
+        array($uid)
+    );
+    OC_DB::executeAudited(
+        'DELETE FROM `*PREFIX*preferences` WHERE `userid` = ?',
+        array($uid)
+    );
+    OC_DB::executeAudited(
+        'DELETE FROM `*PREFIX*group_user` WHERE `uid` = ?',
+        array($uid)
+    );
+    OC_DB::executeAudited(
+        'DELETE FROM `*PREFIX*users_ispconfig` WHERE `uid` = ?',
+        array($uid)
+    );
+    return true;
+  }
 
-	/**
-	 * Get display name of the user
-     *
-	 * @param string $uid user ID of the user
-	 * @return string display name
-     * @throws \OC\DatabaseException
-	 */
-	public function getDisplayName($uid) {
-		$user = OC_DB::executeAudited(
-			'SELECT `displayname` FROM `*PREFIX*users_ispconfig`'
-			. ' WHERE `uid` = ?',
-			array($uid)
-		)->fetchRow();
-		$displayName = trim($user['displayname'], ' ');
-		if (!empty($displayName)) {
-			return $displayName;
-		} else {
-			return $uid;
-		}
-	}
+  /**
+   * Get display name of the user
+   *
+   * @param string $uid user ID of the user
+   * @return string display name
+   * @throws \OC\DatabaseException
+   */
+  public function getDisplayName($uid)
+  {
+    $user = OC_DB::executeAudited(
+        'SELECT `displayname` FROM `*PREFIX*users_ispconfig`'
+        . ' WHERE `uid` = ?',
+        array($uid)
+    )->fetchRow();
+    $displayName = trim($user['displayname'], ' ');
+    if (!empty($displayName)) {
+      return $displayName;
+    } else {
+      return $uid;
+    }
+  }
 
-    /**
-     * Get data of returning user by uid or mailaddress
-     *
-     * @param string $loginName Login Name to check
-     * @return array uid, mailbox, domain
-     * @throws \OC\DatabaseException
-     */
-    public function getUserData($loginName) {
-        $user = OC_DB::executeAudited(
-            'SELECT `uid`, `mailbox`, `domain` FROM `*PREFIX*users_ispconfig`'
-            . ' WHERE `uid` = ? OR CONCAT(`mailbox`, "@", `domain`) = ?',
-            array($loginName, $loginName)
-        )->fetchRow();
-        return $user;
+  /**
+   * Get data of returning user by uid or mailaddress
+   *
+   * @param string $loginName Login Name to check
+   * @return array uid, mailbox, domain
+   * @throws \OC\DatabaseException
+   */
+  public function getUserData($loginName)
+  {
+    $user = OC_DB::executeAudited(
+        'SELECT `uid`, `mailbox`, `domain` FROM `*PREFIX*users_ispconfig`'
+        . ' WHERE `uid` = ? OR CONCAT(`mailbox`, "@", `domain`) = ?',
+        array($loginName, $loginName)
+    )->fetchRow();
+    return $user;
+  }
+
+  /**
+   * Get a list of all display names and user ids.
+   *
+   * @return array with all displayNames (value) and the corresponding uids (key)
+   * @throws \OC\DatabaseException
+   */
+  public function getDisplayNames($search = '', $limit = null, $offset = null)
+  {
+    $result = OC_DB::executeAudited(
+        array(
+            'sql' => 'SELECT `uid`, `displayname` FROM `*PREFIX*users_ispconfig`'
+                . ' WHERE (LOWER(`displayname`) LIKE LOWER(?) '
+                . ' OR LOWER(`uid`) LIKE LOWER(?))',
+            'limit' => $limit,
+            'offset' => $offset
+        ),
+        array('%' . $search . '%', '%' . $search . '%')
+    );
+
+    $displayNames = array();
+    while ($row = $result->fetchRow()) {
+      $displayNames[$row['uid']] = $row['displayname'];
     }
 
-	/**
-	 * Get a list of all display names and user ids.
-	 *
-	 * @return array with all displayNames (value) and the corresponding uids (key)
-     * @throws \OC\DatabaseException
-	 */
-	public function getDisplayNames($search = '', $limit = null, $offset = null) {
-		$result = OC_DB::executeAudited(
-			array(
-				'sql' => 'SELECT `uid`, `displayname` FROM `*PREFIX*users_ispconfig`'
-					. ' WHERE (LOWER(`displayname`) LIKE LOWER(?) '
-					. ' OR LOWER(`uid`) LIKE LOWER(?))',
-				'limit'  => $limit,
-				'offset' => $offset
-			),
-			array('%' . $search . '%', '%' . $search . '%')
-		);
+    return $displayNames;
+  }
 
-		$displayNames = array();
-		while ($row = $result->fetchRow()) {
-			$displayNames[$row['uid']] = $row['displayname'];
-		}
+  /**
+   * Get a list of all users
+   *
+   * @return array with all uids
+   * @throws \OC\DatabaseException
+   */
+  public function getUsers($search = '', $limit = null, $offset = null)
+  {
+    $result = OC_DB::executeAudited(
+        array(
+            'sql' => 'SELECT `uid` FROM `*PREFIX*users_ispconfig`'
+                . ' WHERE LOWER(`uid`) LIKE LOWER(?)',
+            'limit' => $limit,
+            'offset' => $offset
+        ),
+        array($search . '%')
+    );
+    $users = array();
+    while ($row = $result->fetchRow()) {
+      $users[] = $row['uid'];
+    }
+    return $users;
+  }
 
-		return $displayNames;
-	}
+  /**
+   * Determines if the backend can enlist users
+   *
+   * @return bool
+   */
+  public function hasUserListings()
+  {
+    return true;
+  }
 
-	/**
-	* Get a list of all users
-	*
-	* @return array with all uids
-    * @throws \OC\DatabaseException
-	*/
-	public function getUsers($search = '', $limit = null, $offset = null) {
-		$result = OC_DB::executeAudited(
-			array(
-				'sql' => 'SELECT `uid` FROM `*PREFIX*users_ispconfig`'
-					. ' WHERE LOWER(`uid`) LIKE LOWER(?)',
-				'limit' => $limit,
-				'offset' => $offset
-			),
-			array($search . '%')
-		);
-		$users = array();
-		while ($row = $result->fetchRow()) {
-			$users[] = $row['uid'];
-		}
-		return $users;
-	}
+  /**
+   * Change the display name of a user
+   *
+   * @param string $uid The username
+   * @param string $displayName The new display name
+   *
+   * @return true/false
+   * @throws \OC\DatabaseException
+   */
+  public function setDisplayName($uid, $displayName)
+  {
+    if (!$this->userExists($uid)) {
+      return false;
+    }
+    OC_DB::executeAudited(
+        'UPDATE `*PREFIX*users_ispconfig` SET `displayname` = ?'
+        . ' WHERE LOWER(`uid`) = ?',
+        array($displayName, $uid)
+    );
+    return true;
+  }
 
-	/**
-	 * Determines if the backend can enlist users
-	 *
-	 * @return bool
-	 */
-	public function hasUserListings() {
-		return true;
-	}
+  /**
+   * Create user record in database
+   *
+   * @param string $uid The username
+   * @param string $displayname Users displayname
+   * @param string|bool $quota Amount of quota for new created user or false
+   * @param array|bool $groups string-array of groups for new created user or false
+   *
+   * @return void
+   * @throws \OC\DatabaseException
+   */
+  protected function storeUser($uid, $mailbox, $domain, $displayname, $quota = false, $groups = false)
+  {
+    if (!$this->userExists($uid)) {
+      OC_DB::executeAudited(
+          'INSERT INTO `*PREFIX*users_ispconfig` ( `uid`, `displayname`, `mailbox`, `domain` )'
+          . ' VALUES( ?, ?, ?, ? )',
+          array($uid, $displayname, $mailbox, $domain)
+      );
 
-	/**
-	 * Change the display name of a user
-	 *
-	 * @param string $uid         The username
-	 * @param string $displayName The new display name
-	 *
-	 * @return true/false
-     * @throws \OC\DatabaseException
-	 */
-	public function setDisplayName($uid, $displayName) {
-		if (!$this->userExists($uid)) {
-			return false;
-		}
-		OC_DB::executeAudited(
-			'UPDATE `*PREFIX*users_ispconfig` SET `displayname` = ?'
-			. ' WHERE LOWER(`uid`) = ?',
-			array($displayName, $uid)
-		);
-		return true;
-	}
+      $this->setInitialUserProfile($uid, "$mailbox@$domain", $displayname);
+      if ($quota)
+        $this->setUserQuota($uid, $quota);
+      if ($groups)
+        foreach ($groups AS $gid) {
+          $this->addUserToGroup($uid, $gid);
+        }
+    }
+  }
 
-	/**
-	 * Create user record in database
-	 *
-	 * @param string $uid The username
-     * @param string $displayname Users displayname
-     * @param string|bool $quota Amount of quota for new created user or false
-     * @param array|bool $groups string-array of groups for new created user or false
-	 *
-	 * @return void
-     * @throws \OC\DatabaseException
-	 */
-	protected function storeUser($uid, $mailbox, $domain, $displayname, $quota = false, $groups = false)
-	{
-		if (!$this->userExists($uid)) {
-			OC_DB::executeAudited(
-				'INSERT INTO `*PREFIX*users_ispconfig` ( `uid`, `displayname`, `mailbox`, `domain` )'
-				. ' VALUES( ?, ?, ?, ? )',
-				array($uid, $displayname, $mailbox, $domain)
-			);
-
-            $this->setInitialUserProfile($uid, "$mailbox@$domain", $displayname);
-			if ($quota)
-			    $this->setUserQuota($uid, $quota);
-			if($groups)
-                foreach($groups AS $gid) {
-                    $this->addUserToGroup($uid, $gid);
-                }
-		}
-	}
-
-	/**
-	 * Check if a user exists
-	 *
-	 * @param string $uid the username
-	 *
-	 * @return boolean
-     * @throws \OC\DatabaseException
-	 */
-	public function userExists($uid) {
-		$result = OC_DB::executeAudited(
-			'SELECT COUNT(*) FROM `*PREFIX*users_ispconfig`'
-			. ' WHERE LOWER(`uid`) = LOWER(?)',
-			array($uid)
-		);
-		return $result->fetchOne() > 0;
-	}
+  /**
+   * Check if a user exists
+   *
+   * @param string $uid the username
+   *
+   * @return boolean
+   * @throws \OC\DatabaseException
+   */
+  public function userExists($uid)
+  {
+    $result = OC_DB::executeAudited(
+        'SELECT COUNT(*) FROM `*PREFIX*users_ispconfig`'
+        . ' WHERE LOWER(`uid`) = LOWER(?)',
+        array($uid)
+    );
+    return $result->fetchOne() > 0;
+  }
 
   /**
    * @param string $uid the username
@@ -219,87 +230,91 @@ abstract class Base extends \OC\User\Backend{
    * @param string $value value to save for the users preference
    * @throws \OC\DatabaseException
    */
-	private function setUserPreference($uid, $appid, $configkey, $value) {
-        OC_DB::executeAudited('INSERT INTO `*PREFIX*preferences` (`userid`, `appid`, `configkey`, `configvalue`)'
-            . ' VALUES (?, ?, ?, ?)',
-            array($uid, $appid, $configkey, $value)
-        );
-    }
+  private function setUserPreference($uid, $appid, $configkey, $value)
+  {
+    OC_DB::executeAudited('INSERT INTO `*PREFIX*preferences` (`userid`, `appid`, `configkey`, `configvalue`)'
+        . ' VALUES (?, ?, ?, ?)',
+        array($uid, $appid, $configkey, $value)
+    );
+  }
 
-    /**
-     * @param string $uid the username
-     * @param string $quota amount of quota
-     * @throws \OC\DatabaseException
-     */
-	private function setUserQuota($uid, $quota) {
-        OC_DB::executeAudited('INSERT INTO `*PREFIX*preferences` (`userid`, `appid`, `configkey`, `configvalue`)'
-            . ' VALUES (?, ?, ?, ?)',
-            array($uid, 'files', 'quota', $quota)
-        );
-    }
+  /**
+   * @param string $uid the username
+   * @param string $quota amount of quota
+   * @throws \OC\DatabaseException
+   */
+  private function setUserQuota($uid, $quota)
+  {
+    OC_DB::executeAudited('INSERT INTO `*PREFIX*preferences` (`userid`, `appid`, `configkey`, `configvalue`)'
+        . ' VALUES (?, ?, ?, ?)',
+        array($uid, 'files', 'quota', $quota)
+    );
+  }
 
-    /**
-     * Add user to group
-     *
-     * @param string $uid the username
-     * @param string $gid the groupname
-     * @throws \OC\DatabaseException
-     */
-    protected function addUserToGroup($uid, $gid){
-        OC_DB::executeAudited(
-            'INSERT INTO `*PREFIX*group_user` (`gid`, `uid`) VALUES (?, ?)',
-            array($gid, $uid)
-        );
-    }
+  /**
+   * Add user to group
+   *
+   * @param string $uid the username
+   * @param string $gid the groupname
+   * @throws \OC\DatabaseException
+   */
+  protected function addUserToGroup($uid, $gid)
+  {
+    OC_DB::executeAudited(
+        'INSERT INTO `*PREFIX*group_user` (`gid`, `uid`) VALUES (?, ?)',
+        array($gid, $uid)
+    );
+  }
 
-    /**
-     * Add user to group
-     *
-     * @param string $uid the username
-     * @param string $email users mail address
-     * @param string $displayname users real name
-     * @throws \OC\DatabaseException
-     */
-    private function setInitialUserProfile($uid, $email, $displayname) {
-        $this->setUserPreference($uid,'settings', 'email', $email);
-        OC_DB::executeAudited(
-            'INSERT INTO `*PREFIX*accounts` ( `uid`, `data` )'
-            . ' VALUES( ?, ? )',
-            array($uid, json_encode(array(
-                'displayname' => array(
-                    'value' => $displayname,
-                    'scope' => 'contacts',
-                    'verified' => 0
-                ),
-                'address' => array(
-                    'value' => '',
-                    'scope' => 'private',
-                    'verified' => 0
-                ),
-                'website' => array(
-                    'value' => '',
-                    'scope' => 'private',
-                    'verified' => 0
-                ),
-                'email' => array(
-                    'value' => $email,
-                    'scope' => 'contacts',
-                    'verified' => 0
-                ),
-                'avatar' => array(
-                    'scope' => 'contacts',
-                ),
-                'phone' => array(
-                    'value' => '',
-                    'scope' => 'private',
-                    'verified' => 0
-                ),
-                'twitter' => array(
-                    'value' => '',
-                    'scope' => 'private',
-                    'verified' => 0
-                ),
-            )))
-        );
-    }
+  /**
+   * Add user to group
+   *
+   * @param string $uid the username
+   * @param string $email users mail address
+   * @param string $displayname users real name
+   * @throws \OC\DatabaseException
+   */
+  private function setInitialUserProfile($uid, $email, $displayname)
+  {
+    $this->setUserPreference($uid, 'settings', 'email', $email);
+    OC_DB::executeAudited(
+        'INSERT INTO `*PREFIX*accounts` ( `uid`, `data` )'
+        . ' VALUES( ?, ? )',
+        array($uid, json_encode(array(
+            'displayname' => array(
+                'value' => $displayname,
+                'scope' => 'contacts',
+                'verified' => 0
+            ),
+            'address' => array(
+                'value' => '',
+                'scope' => 'private',
+                'verified' => 0
+            ),
+            'website' => array(
+                'value' => '',
+                'scope' => 'private',
+                'verified' => 0
+            ),
+            'email' => array(
+                'value' => $email,
+                'scope' => 'contacts',
+                'verified' => 0
+            ),
+            'avatar' => array(
+                'scope' => 'contacts',
+            ),
+            'phone' => array(
+                'value' => '',
+                'scope' => 'private',
+                'verified' => 0
+            ),
+            'twitter' => array(
+                'value' => '',
+                'scope' => 'private',
+                'verified' => 0
+            ),
+        )))
+    );
+  }
 }
