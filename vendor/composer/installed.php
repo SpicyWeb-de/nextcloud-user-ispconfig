f4c313d6e737f627b6b2d8fd7aac21091c4d6452
Michael Fürmann <michael@fuermann.net>
2023-09-26 23:05:40 +0200

    #13 Add nusaop library to project

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b751e5513927bc700e974577f832bbd47bb22ee0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b751e5513927bc700e974577f832bbd47bb22ee0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'econea/nusoap' => array(
+            'pretty_version' => 'v0.9.16',
+            'version' => '0.9.16.0',
+            'reference' => '9ead68ec7ad8d1e14943658ce1559435247d14bf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../econea/nusoap',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
